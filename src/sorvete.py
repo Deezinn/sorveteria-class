e5315ccf9f9881500afcb018da64f073791d8345
Deezinn <developer.andrelz@gmail.com>
2025-05-22 18:15:47 -0300

    feat: add property in mother class

--- src/sorvete.py
+++ src/sorvete.py
@@ -4,5 +4,15 @@ class Sorvete:
         self._suporte = suporte
         self._cobertura = cobertura
 
-    
-        
+    @property
+    def preco(self):
+        return self._preco
+
+    @property
+    def suporte(self):
+        return self._suporte
+
+    @property
+    def cobertura(self):
+        return self._cobertura
+
