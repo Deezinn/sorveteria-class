572619a28aff50283d93382afda1387d9fd3afd3
Deezinn <developer.andrelz@gmail.com>
2025-05-22 18:18:44 -0300

    refix: refatoração

--- src/sorvete_premium.py
+++ src/sorvete_premium.py
@@ -1,14 +1,12 @@
 from sorvete import Sorvete
 
 class SorvetePremium(Sorvete):
-    def __init__(self, preco=15, suporte=None, cobertura=None):
-        super().__init__(preco, suporte, cobertura)
+    def __init__(self, suporte=None, cobertura=None):
+        super().__init__(preco=15, suporte=suporte, cobertura=cobertura)
 
     @property
     def sorvete_casquinha(self):
-        self._preco += 2
-        self._suporte = 'casquinha'
-        return self._preco, self._suporte
+        return self._preco + 2, 'casquinha'
 
     @sorvete_casquinha.setter
     def sorvete_casquinha(self,_):
@@ -17,12 +15,9 @@ class SorvetePremium(Sorvete):
 
     @property
     def sorvete_copinho(self):
-        self._preco += 1
-        self._suporte = 'copinho'
-        return self._preco, self._suporte
+        return self._preco + 2, 'copinho'
 
     @sorvete_copinho.setter
     def sorvete_copinho(self,_):
         self._preco += 1
         self._suporte = 'copinho'
-
